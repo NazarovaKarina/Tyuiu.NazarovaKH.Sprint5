fd800bbc168b030b54bc0a0b1f6ca668d9006668
Tyuiu.NazarovaKKH@outlook.com <Tyuiu.NazarovaKKH@outlook.com>
2025-01-02 15:56:50 +0300

    Task0

--- Tyuiu.NazarovaKH.Sprint5.Task0.V7.Lib/DataService.cs
+++ Tyuiu.NazarovaKH.Sprint5.Task0.V7.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.NazarovaKH.Sprint5.Task0.V7.Lib
         public string SaveToFileTextData(int x)
         {
             double y = Convert.ToDouble(x);
-            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask0.txt" });
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask0.txt" }); 
             double z = (-(Math.Pow(y, 3))) + 4 * (Math.Pow(y, 2)) - (3.0 / 2.0 * y);
             z = Math.Round(z, 3);
             File.WriteAllText(path, Convert.ToString(z));
